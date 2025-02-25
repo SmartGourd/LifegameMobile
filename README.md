5fde384e96b92eb18ca7d415837d89b94a32a808
Ladislav Nagy <nagy.ladik@gmail.com>
2025-02-25 15:22:00 +0100

    Add readme and development tips and remove one import

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lifegame mobile
+A mobile application for participating in a Race game
+Allows to play in Connecting or Writing mode to learn pair associations
+The game needs to be created in the web application at https://lifegame.site/lobby
--- src/main/java/cz/zlehcito/ui/pages/GamePage.kt
+++ src/main/java/cz/zlehcito/ui/pages/GamePage.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
